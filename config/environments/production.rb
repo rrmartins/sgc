827ea4409a17daf86ee6ea5aece0de7a81f2339f
rrmartins <rodrigo@rrmartins.com>
2012-07-12 19:15:16 -0300

    add configuration precompile in enviroment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,13 @@ Sgc::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+    # Prevent initializing your application and connecting to the database
+    # config.assets.initialize_on_precompile = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
